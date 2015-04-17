ff5b7d49e4e3744bab13fdfc51c4aa6450454b3f
Johanw123 <johanw123@gmail.com>
2015-04-17 23:36:11 +0200

    Initial Commit

---
+++ Assets/EmptyScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyScrip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
