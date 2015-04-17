15e9f106029ec6d4687710337c5918c389d96c99
kopa <kopatheonlyone@hotmail.com>
2015-04-18 00:19:59 +0200

    Emptied the script
    It's empty

--- Assets/EmptyScript.cs
+++ Assets/EmptyScript.cs
@@ -3,18 +3,5 @@ using System.Collections;
 
 public class EmptyScrip : MonoBehaviour 
 {
-<<<<<<< HEAD
-    
-=======
-  //Empty Script that does nothing ;(
-	void Start () 
-  {
-	  
-	}
 
-	void Update () 
-  {
-
-	}
->>>>>>> origin/master
 }
