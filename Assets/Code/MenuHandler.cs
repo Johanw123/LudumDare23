f1263899ac2cf9f053aa7de7a69f22e7f33a4fe3
Johanw123 <johanw123@gmail.com>
2015-04-18 18:10:52 +0200

    Main Menu 2.0
    yeah baby

---
+++ Assets/Code/MenuHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour 
+{
+  public void OnExitGame()
+  {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+  }
+
+  public void OnPlayGame()
+  {
+    Application.LoadLevel("basicLevel"); 
+  }
+
+  public Menu CurrentMenu;
+
+  void Start()
+  {
+    ShowMenu(CurrentMenu);
+  }
+
+  public void ShowMenu(Menu menu)
+  {
+    Debug.Log("SHow Menu: " + menu);
+
+    if (CurrentMenu != null)
+      CurrentMenu.IsOpen = false;
+
+    CurrentMenu = menu;
+    CurrentMenu.IsOpen = true;
+  }
+
+
+}
