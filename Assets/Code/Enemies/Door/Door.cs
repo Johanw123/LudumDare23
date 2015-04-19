1d2d7b07ec5f8520c4a20d109a55729e87c4bea1
Johanw123 <johanw123@gmail.com>
2015-04-19 22:48:02 +0200

    The door speaks!

--- Assets/Code/Enemies/Door/Door.cs
+++ Assets/Code/Enemies/Door/Door.cs
@@ -1,16 +1,36 @@
 ﻿    using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Door :Enemy 
 {
+    private GameObject Player;
+
+    private Text text;
+
     public override void Start()
     {
         spriRen = GetComponent<SpriteRenderer>();
+        text = GameObject.Find("Door Text").GetComponent<Text>();
+        Player = GameObject.Find("Player");
     }
 
     public override void Update()
     {
-        base.Update();
+      if (text != null)
+      {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if (distance < 1.5f)
+        {
+          text.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+          text.text = "Hello, my name is Door... I will not let you enter. Try something 'Unconventional'.";
+        }
+        else
+          text.text = "";
+      }
+
+      base.Update();
     }
 
     public override void FixedUpdate()
