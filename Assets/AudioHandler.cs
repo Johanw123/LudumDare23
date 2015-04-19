9cd9a82ae0405a329acf47b3482854f7426e9121
Johanw123 <johanw123@gmail.com>
2015-04-19 10:48:54 +0200

    Added so music volume slider affects volume of background music

---
+++ Assets/AudioHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioHandler : MonoBehaviour 
+{
+  public static float SfxVolume = 0.5f;
+  public static float MusicVolume = 0.5f;
+
+  public AudioSource BackgroundMusic;
+
+  public static void SetMusicVolume(float volume)
+  {
+    MusicVolume = volume;
+  }
+
+  public static void SetSfxVolume(float sfx)
+  {
+    SfxVolume = sfx;
+    AudioListener.volume = SfxVolume;
+  }
+
+	void Start() 
+  {
+    SetSfxVolume(SfxVolume);
+    SetMusicVolume(MusicVolume);
+
+    BackgroundMusic.volume = MusicVolume;
+    BackgroundMusic.ignoreListenerVolume = true;
+    BackgroundMusic.Play();
+	}
+}
--- Assets/VolumeHandler.cs
+++ Assets/VolumeHandler.cs
@@ -8,6 +8,7 @@ public class VolumeHandler : MonoBehaviour
   Slider m_volumeSlider;
   string m_startingText;
   Color m_startingColor;
+  bool m_isMusic;
 
 	void Start() 
   {
@@ -16,12 +17,30 @@ public class VolumeHandler : MonoBehaviour
 
     m_startingColor = m_volumeText.color;
     m_startingText = m_volumeText.text;
+    m_isMusic = m_startingText.Contains("Music");
+
+    m_volumeSlider.value = m_isMusic ? AudioHandler.MusicVolume * 100 : AudioHandler.SfxVolume * 100;
+
     SetVolumeText();
 	}
 
   public void OnVolumeChanged()
   {
+    SetVolume(m_volumeSlider.value);
     SetVolumeText();
+
+    UpdateMenuMusicVolume();
+  }
+
+  private void UpdateMenuMusicVolume()
+  {
+    var gameObject = GameObject.Find("/Music");
+    if(gameObject != null)
+    {
+      var audioSource = gameObject.GetComponent<AudioSource>();
+      if (audioSource != null)
+        audioSource.volume = m_volumeSlider.value / 100;
+    }
   }
 
   public void OnToggle()
@@ -29,10 +48,25 @@ public class VolumeHandler : MonoBehaviour
     m_volumeSlider.enabled = !m_volumeSlider.enabled;
 
     if (m_volumeSlider.enabled)
+    {
       m_volumeText.color = m_startingColor;
+      SetVolume(m_volumeSlider.value);
+    }
     else
+    {
       m_volumeText.color = new Color(0.5f, 0.5f, 0.5f);
-      
+      SetVolume(0);
+    }
+  }
+
+  private void SetVolume(float value)
+  {
+    float convertedValue = value / 100;
+
+    if (m_isMusic)
+      AudioHandler.SetMusicVolume(convertedValue);
+    else
+      AudioHandler.SetSfxVolume(convertedValue);
   }
 
   private void SetVolumeText()
