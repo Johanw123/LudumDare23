757a6d71401cc34c219b98592033617b6fed7680
Sam Rowlands <srowlands@hotmail.co.uk>
2015-04-19 10:54:56 +0100

    LevelOne.

---
+++ Assets/Code/Enemies/Door/Door.cs
@@ -0,0 +1,20 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class Door :Enemy 
+{
+    public override void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+}
